98ae302422b4969d196b42a52af154dc6a43bb79
Pete LePage <pete.lepage@pobox.com>
2011-11-17 17:05:22 -0800

    layout

--- sources/static/js/script.js
+++ sources/static/js/script.js
@@ -2,7 +2,32 @@
 
 */
 
+var Hipster = (function(){
+ 
+  $(document).ready(function(){
 
-
-
+  });
+  
+  // google analytics
+  var _gaq=[['_setAccount','UA-9988000-4'],['_trackPageview'],['_trackPageLoadTime']];
+  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js?v=2';
+  s.parentNode.insertBefore(g,s)}(document,'script'));
+  
+	// Create a private variable
+	//var name = "Spock";
+ 
+	// Create a private function
+	//var pvtf = function() {
+	//	return "Hello " + name;
+	//};
+	
+	// Create a public function
+	//this.pubf = function() {
+	// return "yes";
+	//};
+ 
+	return this;
+ 
+})();
 
